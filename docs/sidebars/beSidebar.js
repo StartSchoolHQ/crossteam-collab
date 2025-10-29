9d34f820ceb18115872831a5f1500301170a7a7d
StartSchool <start@startschool.org>
2025-10-29 12:50:19 +0200

    BE session content division in chapters

--- docs/sidebars/beSidebar.js
+++ docs/sidebars/beSidebar.js
@@ -7,7 +7,14 @@ export const beSidebar = [
       },
       items: [
         'BE/be-intro',
-     
+        'BE/be-db',
+        'BE/be-sec',
+        'BE/be-lang',
+        'BE/be-api',
+        'BE/be-patterns',
+        'BE/be-scen',
+        'BE/be-key',
+        
     //     {
     //         type: 'category',
     //         label: 'TASK 1 | ',
